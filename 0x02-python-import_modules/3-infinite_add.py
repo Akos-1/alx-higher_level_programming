cc7c415c76caf3fdb383e354199f71e0aec9a9c4
Akos-1 <veronicagyasi@gmail.com>
2023-07-13 21:55:28 +0000

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 import sys
+
 def add_arguments(arguments):
     total = 0
     for arg in arguments:
         total += int(arg)
     return total
 
-if __name__ == "__main__":
     arguments = sys.argv[1:]
     result = add_arguments(arguments)
     print(result)
