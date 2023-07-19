443d8a6e77ef6c32183aadd941e0f2f5d4515eef
Akos-1 <veronicagyasi@gmail.com>
2023-07-19 12:36:38 +0000

    matrix simple

--- 0-square_matrix_simple.py
+++ 0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-        new_row.append(element **2)
-    squared_matrix.append(new_row)
-return square_matrix
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+return new_matrix
