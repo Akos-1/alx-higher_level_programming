4189be9341444af486802b0234c8a145cf014835
Akos-1 <veronicagyasi@gmail.com>
2023-08-01 21:39:53 +0000

    singly

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -19,19 +19,23 @@ class Node:
     @property
     def data(self):
         return (self.__data)
+
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
+
     @property
     def next_node(self):
         return (self.__next_node)
+
     @next_node.setter
     def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """Represent a singly-linked list."""
