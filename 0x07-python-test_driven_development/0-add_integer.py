b169e645fe22849e1df833c11a657af050f7a79a
Akos-1 <veronicagyasi@gmail.com>
2023-08-08 22:11:39 -0700

    tdd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an integer addition functions"""
+
+
 def add_integer(a, b=98):
     """
     Adds two integers.
@@ -13,12 +16,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance (b, float))):
+        raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-
-    return (a + b);
+    return (int(a) + int(b));
