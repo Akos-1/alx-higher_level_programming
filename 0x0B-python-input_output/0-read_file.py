9ab577856bb35b3e1962181ee828cebcc3c21f8a
Akos-1 <veronicagyasi@gmail.com>
2023-08-15 15:05:48 -0700

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file """
+
+
+def read_file(filename=""):
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding='utf-8') as f:
+            content = f.read()
+            print(content, end ="")
