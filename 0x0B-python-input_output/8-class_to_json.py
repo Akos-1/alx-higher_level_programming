18ed4b1979a1398e08d8516255f6a5bffcc03a73
Akos-1 <veronicagyasi@gmail.com>
2023-08-16 22:06:02 -0700

    a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean)

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines abclass to JSON function"""
+
+
+def class_to_json(obj):
+    """" returns the dictionary description with simple data structure"""
+    return obj._dict_
