09a7c35ab883a59424f7cfc25537d4ce004ab299
Akos-1 <veronicagyasi@gmail.com>
2023-08-21 19:37:19 +0000

    the class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,12 @@ import json
 
 class Base:
     """The Base model
+
     This is the "base" for all the classes in this projects"
 
     Private Class Attributes:
         __nb_object (int):
-        Number of instantiated Bases.
+Number of instantiated Bases.
     """
 
     __nb_objects = 0
