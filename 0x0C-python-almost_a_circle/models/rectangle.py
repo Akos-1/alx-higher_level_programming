36e8245e463932807da84e6a2d274dd11f631915
Akos-1 <veronicagyasi@gmail.com>
2023-08-21 11:25:27 +0000

    hat prints in stdout the Rectangle instance with the character #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,5 +70,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
     def area(self):
         return self.__width * self.__height
+
+    def display(self):
+        for _ in range(self.__height):
+            print('#' * self.__width)
