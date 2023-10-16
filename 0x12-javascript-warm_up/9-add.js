efdf449c58cab99292c87361b35e25d171dc19e9
Akos-1 <veronicagyasi@gmail.com>
2023-10-16 15:08:12 -0700

    errors fixed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 function add(a, b) {
-  return a + b;
+  const c =a + b;
+  console.log(c);	
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  const result = add(num1, num2);
-  console.log(result);
-}
+add(Number(process.argv[2]),Number(process.agv[3]));
