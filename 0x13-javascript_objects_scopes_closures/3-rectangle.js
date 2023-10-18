27d45caafecf8a1efba90ccbe3b784597362fb91
Akos-1 <veronicagyasi@gmail.com>
2023-10-18 14:09:15 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
-    }
   }
 
-  print() {
-    if (Object.keys(this).length !== 0) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
+      console.log(s);
     }
   }
 }
