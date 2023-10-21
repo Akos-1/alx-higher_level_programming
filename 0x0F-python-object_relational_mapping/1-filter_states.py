9d82b7170f7bef975d720dc90290d6b3e5736b9c
Akos-1 <veronicagyasi@gmail.com>
2023-10-21 10:40:39 -0700

    error fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N from the database hbtn_0e_0_usa
+Script that lists all states with a name starting
+with N from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
@@ -18,7 +19,14 @@ if __name__ == "__main__":
         db=database_name
     )
     cur = db.cursor()
-    cur.execute("SELECT MIN(id), name FROM states WHERE name LIKE 'N%' GROUP BY name ORDER BY MIN(id) ASC")
+    query = (
+        "SELECT MIN(id), name "
+        "FROM states "
+        "WHERE name LIKE 'N%' "
+        "GROUP BY name "
+        "ORDER BY MIN(id) ASC"
+    )
+    cur.execute(query)
     states = cur.fetchall()
     for state in states:
         print(state)
