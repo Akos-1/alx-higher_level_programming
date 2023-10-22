bcaa4f20fedd6f2652814687609565c2ec3afedc
Akos-1 <veronicagyasi@gmail.com>
2023-10-22 13:55:06 -0700

    errors to be worked on

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Script that displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    database_name = argv[3]
-    state_name = argv[4]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,9 +23,9 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+                .format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
