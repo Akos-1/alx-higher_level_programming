a0881ea19c2fda550c3b445fa82c49311f19f5e4
Akos-1 <veronicagyasi@gmail.com>
2023-10-22 14:57:51 -0700

    updated

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+       print("Error: Insufficient arguments.")
+    sys.exit(1)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    states = cur.fetchall()
+    for s in states:
+        print(s)
+    cur.close()
+    db.close()
