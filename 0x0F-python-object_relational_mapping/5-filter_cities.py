012e7228d840a14ee5adb90416c2b78dddacd9d7
Akos-1 <veronicagyasi@gmail.com>
2023-10-26 13:48:59 -0700

    updated

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all cities from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    if len(argv) != 5:
-        print("Usage: {} username password database state".format(argv[0]))
-    else:
-        mysql_username = argv[1]
-        mysql_password = argv[2]
-        database_name = argv[3]
-        state_name = argv[4]
+    mysql_username, mysql_password, database_name, state_name = sys.argv[1:5]
 
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
-        cursor = db.cursor()
-        query = "SELECT cities.id, states.name, cities.name FROM cities \
-                 JOIN states ON cities.state_id = states.id \
-                 WHERE states.name = %s ORDER BY cities.id ASC"
-        cursor.execute(query, (state_name,))
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-        db.close()
+    db = MySQLdb.connect(host="localhost", user=mysql_username,
+                         passwd=mysql_password, db=database_name, port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities INNER JOIN states ON
+                states.id=cities.state_id WHERE states.name=%s""",
+                (state_name,))
+    rows = cur.fetchall()
+    cities = [row[0] for row in rows]
+    print(*cities, sep=", ")
+    cur.close()
+    db.close()
