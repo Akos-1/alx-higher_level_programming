45bc82072d1e9f535b781169e6298ce41d2a0ca1
Akos-1 <veronicagyasi@gmail.com>
2023-10-26 13:52:12 -0700

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
-    else:
-        mysql_username = argv[1]
-        mysql_password = argv[2]
-        database_name = argv[3]
-
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-        cursor.close()
-        db.close()
+    mysql_username, mysql_password, database_name = sys.argv[1:4]
+    db = MySQLdb.connect(host="localhost", user=mysql_username,
+                         passwd=mysql_password, db=database_name, port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                   cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
