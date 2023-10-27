3ffc3f2da8d9bff323ef31a70f9288a766e3126e
Akos-1 <veronicagyasi@gmail.com>
2023-10-26 22:07:09 -0700

    to be worked on

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the specified
+name from the database hbtn_0e_6_usa.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    mysql_username, mysql_password, database_name, state_name = argv[1:5]
+
+    connection_url = (
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@'
+        f'localhost:3306/{database_name}'
+    )
+    engine = create_engine(connection_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    searched_state = session.query(State).filter(State.name == state_name).first()
+    if searched_state:
+        print(searched_state.id)
+    else:
+        print('Not found')
+    session.close()
