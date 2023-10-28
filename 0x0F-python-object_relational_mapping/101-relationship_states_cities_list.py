e14a27e3536c639dbb53a1f90b968ae0ee2b66fd
Akos-1 <veronicagyasi@gmail.com>
2023-10-28 05:11:34 -0700

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Prints the State object with the name passed as an
-argument from the database
+""" Prints the State object with the name passed
+as an argument from the database
 """
 import sys
 from relationship_state import Base, State
