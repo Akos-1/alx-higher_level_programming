b6e0e706e48d28fb3e0dafde92029e07e49e7371
Akos-1 <veronicagyasi@gmail.com>
2023-10-28 05:20:02 -0700

    update

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     for instance in session.query(State).order_by(State.id):
         for city_ins in instance.cities:
             print(f"{city_ins.id}: {city_ins.name} -> {instance.name}")
