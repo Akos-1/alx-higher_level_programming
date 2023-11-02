b72cd16d55b9933f071449b80a59c8a94e4e94ee
Akos-1 <veronicagyasi@gmail.com>
2023-11-02 14:47:14 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument
-#sends a silent request,
-#and displays the size of the response body in bytes.
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
+# This script takes a URL as an argument, sends a silent request, and displays the size of the response body in bytes.
 curl -sI "$1" | grep -iE '^Content-Length:' | awk '{print $2}' | tr -d '\r'
