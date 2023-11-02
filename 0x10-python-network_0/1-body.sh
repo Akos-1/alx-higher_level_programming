7c80c7b420810e171262c0bca2c6dc08999e35c7
Akos-1 <veronicagyasi@gmail.com>
2023-11-02 15:08:07 -0700

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request, and displays the body of the response for a 200 status code.
-curl -s -o /dev/null -w "%{http_code}\n%{body}" "$1" | awk '/^200/{getline; print}'
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1" || echo "Error: Non-200 status code"
