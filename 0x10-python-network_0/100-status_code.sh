feea62f67a7f22ee384bcdd59627d618010f70b1
Akos-1 <veronicagyasi@gmail.com>
2023-11-04 11:56:07 -0700

    update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument and displays only the status code of the response.
-[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; }; curl -s -o response.txt -w "%{http_code}" "$1"; cat response.txt; wc -l < response.txt; rm response.txt
+if [ -z "$1" ]; then echo "Usage: $0 <URL>"; exit 1; fi
+curl -s -o response.txt -w "%{http_code}" "$1"
+cat response.txt
+wc -l < response.txt
+rm response.txt
