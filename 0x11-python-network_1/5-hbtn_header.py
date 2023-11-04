a006bd170937c705da705399c0e066f8ec85c5f1
Akos-1 <veronicagyasi@gmail.com>
2023-11-04 09:50:40 -0700

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,19 @@
 import requests
 import sys
 
+def get_x_request_id(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        return response.headers.get('X-Request-Id', 'Not present')
+    except requests.exceptions.RequestException as e:
+        return f'Error: {e}'
 
-url = sys.argv[1]
 
-response = requests.get(url)
-
-x_request_id = response.headers.get('X-Request-Id', 'Not present')
-
-print(x_request_id)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <url>")
+    else:
+        url = sys.argv[1]
+        x_request_id = get_x_request_id(url)
+        print(x_request_id)
