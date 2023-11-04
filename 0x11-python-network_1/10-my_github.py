ce84d077dd4859df94f501d13043cde41f736ec5
Akos-1 <veronicagyasi@gmail.com>
2023-11-03 18:03:58 -0700

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
+import base64
 
 if len(sys.argv) != 3:
     print("Usage: python script.py <username> <access_token>")
@@ -12,13 +13,14 @@ access_token = sys.argv[2]
 url = "https://api.github.com/user"
 
 headers = {
-    'Authorization': 'Basic {}:{}'.format(username, access_token),
+    'Authorization': 'Basic {}'.format(base64.b64encode(f"{username}:{access_token}".encode()).decode()),
 }
 
 response = requests.get(url, headers=headers)
 
 try:
     data = response.json()
-    print("Your GitHub id:", data['id'])
+    print("GitHub API response:", data)  # Print the entire response
+    print("Your GitHub id:", data.get('id', 'Not available'))
 except ValueError:
     print("Not a valid JSON")
