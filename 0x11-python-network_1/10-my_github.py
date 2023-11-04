08755a9c5c2a89819ebb9b3334ea06058abc30b4
Akos-1 <veronicagyasi@gmail.com>
2023-11-04 10:54:31 -0700

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,32 +2,23 @@
 import requests
 import sys
 
+if len(sys.argv) != 3:
+    print("Usage: python script.py <username> <access_token>")
+    sys.exit(1)
 
-def get_github_id(username, password):
-    url = "https://api.github.com/user"
+username = sys.argv[1]
+access_token = sys.argv[2]
 
-    headers = {
-        'Authorization': 'Basic {}:{}'.format(username, password),
-    }
+url = "https://api.github.com/user"
 
-    response = requests.get(url, headers=headers)
+headers = {
+    'Authorization': 'Basic {}:{}'.format(username, access_token),
+}
 
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            print("Your GitHub id:", data['id'])
-        except ValueError:
-            print("Not a valid JSON")
-    else:
-        print("Error:", response.status_code)
+response = requests.get(url, headers=headers)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <username> <access_token>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    access_token = sys.argv[2]
-
-    get_github_id(username, access_token)
+try:
+    data = response.json()
+    print("Your GitHub id:", data['id'])
+except ValueError:
+    print("Not a valid JSON")
