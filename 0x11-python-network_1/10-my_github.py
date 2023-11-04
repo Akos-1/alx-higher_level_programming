598744ce91cec8801bcbf543a551b21f34f8a6d5
Akos-1 <veronicagyasi@gmail.com>
2023-11-04 10:52:29 -0700

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,23 +2,32 @@
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py <username> <access_token>")
-    sys.exit(1)
 
-username = sys.argv[1]
-access_token = sys.argv[2]
+def get_github_id(username, password):
+    url = "https://api.github.com/user"
 
-url = "https://api.github.com/user"
+    headers = {
+        'Authorization': 'Basic {}:{}'.format(username, password),
+    }
 
-headers = {
-    'Authorization': 'Basic {}:{}'.format(username, access_token),
-}
+    response = requests.get(url, headers=headers)
 
-response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print("Your GitHub id:", data['id'])
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("Error:", response.status_code)
 
-try:
-    data = response.json()
-    print("Your GitHub id:", data['id'])
-except ValueError:
-    print("Not a valid JSON")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <username> <access_token>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    get_github_id(username, access_token)
