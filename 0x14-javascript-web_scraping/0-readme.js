084fef8e3a1f86d8617084abbf8245939d630b07
Akos-1 <veronicagyasi@gmail.com>
2023-11-30 12:23:55 -0800

    fixed some errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,12 @@ const fs = require('fs');
 const readAndPrintFile = (filePath) => {
   try {
     const content = fs.readFileSync(filePath, 'utf-8');
-    console.log(content);
+    console.log('File content:', content); // Log the file content
+    if (content.includes('Error')) {
+      console.log('Error found in file content.');
+    } else {
+      console.log('No error found in file content.');
+    }
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
   }
@@ -15,5 +20,6 @@ if (process.argv.length !== 3) {
   console.log('Usage: ./read_file.js <file_path>');
 } else {
   const filePath = process.argv[2];
+  console.log('Reading file:', filePath);
   readAndPrintFile(filePath);
 }
