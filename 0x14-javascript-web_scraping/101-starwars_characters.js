a45da5fa4f6b82bb229c8a32f947b3763bfd662d
Akos-1 <veronicagyasi@gmail.com>
2023-12-01 00:03:51 -0800

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,6 +6,17 @@ const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 let characters = [];
 
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const characterData = JSON.parse(body);
+  characters = characterData.characters;
+  getCharacters(0);
+});
+
 const getCharacters = (index) => {
   if (index === characters.length) {
     return;
@@ -21,14 +32,3 @@ const getCharacters = (index) => {
     getCharacters(index + 1);
   });
 };
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  characters = data.characters;
-  getCharacters(0);
-});
