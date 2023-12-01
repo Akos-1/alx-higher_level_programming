9ed951e5593630f03db23399a2b43361fd28f5a9
Akos-1 <veronicagyasi@gmail.com>
2023-11-30 23:00:47 -0800

    updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
-const WriteToFile = process.argv[3];
+const contentToWrite = process.argv[3];
 
-if (!filePath || !WriteToFile) {
-  process.exit(1);
-}
-
-try {
-  fs.writeFileSync(filePath, WriteToFile, 'utf-8');
-} catch (error) {
-  console.error(`An error occurred while writing to the file: ${error}`);
-}
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
