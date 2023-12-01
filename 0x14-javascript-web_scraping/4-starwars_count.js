56171b2aa64d720b57c0f847ea12d5b3c7fcce24
Akos-1 <veronicagyasi@gmail.com>
2023-11-30 22:26:27 -0800

    fixed errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -12,22 +12,31 @@ if (!apiUrl) {
 
 const characterUrl = `${apiUrl}${characterId}`;
 
-request(characterUrl, { json: true }, (error, response, body) => {
+request.get(characterUrl, handleApiResponse);
+
+function handleApiResponse(error, response, body) {
   if (error) {
-    console.error(`An error occurred while making the request: ${error}`);
-    return;
+    return handleError(`An error occurred while making the request: ${error}`);
   }
 
-  if (response.statusCode === 200) {
-    try {
-      const characterData = body;
-      const moviesWithCharacter = characterData.films.length;
-
-      console.log(`Number of movies where Wedge Antilles is present: ${moviesWithCharacter}`);
-    } catch (parseError) {
-      console.error(`Error parsing response body: ${parseError}`);
-    }
-  } else {
-    console.error(`Error: Request failed with status code ${response.statusCode}`);
+  if (response.statusCode !== 200) {
+    return handleError(`Error: Request failed with status code ${response.statusCode}`);
   }
-});
+
+  try {
+    const moviesWithCharacter = parseCharacterData(body);
+
+    console.log(`Number of movies where Wedge Antilles is present: ${moviesWithCharacter}`);
+  } catch (parseError) {
+    handleError(`Error parsing response body: ${parseError}`);
+  }
+}
+
+function handleError(message) {
+  console.error(message);
+}
+
+function parseCharacterData(body) {
+  const characterData = JSON.parse(body);
+  return characterData.films.length;
+}
